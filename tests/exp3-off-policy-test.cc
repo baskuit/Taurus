061f3aff682ae7c7b1c0ae6b2af105722a0d4ba4
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-06-17 10:51:47 -0700

    building uncommented tests

--- tests/exp3-off-policy-test.cc
+++ tests/exp3-off-policy-test.cc
@@ -11,25 +11,25 @@ int main()
 
     for (int trial = 0; trial < trials; ++trial)
     {
-        // using Battle = Battle<LogSize>;
-        // using Model = MonteCarloModel<Battle>;
-        // using Algorithm = OffPolicy<Exp3<Model>, MatrixNode, ChanceNode>;
+        using Battle = Battle<LogSize>;
+        using Model = MonteCarloModel<Battle>;
+        using Algorithm = OffPolicy<Exp3<Model>, MatrixNode, ChanceNode>;
 
 
-        // prng device(0);
-        // Battle battle(engine::RBY::miedon, engine::RBY::miedon);
-        // battle.get_actions();
-        // battle.apply_actions(battle.row_actions[0], battle.col_actions[0]);
-        // std::vector<Battle> states{battle};
+        prng device(0);
+        Battle battle(engine::RBY::miedon, engine::RBY::miedon);
+        battle.get_actions();
+        battle.apply_actions(battle.row_actions[0], battle.col_actions[0]);
+        std::vector<Battle> states{battle};
 
 
-        // Model model(device);
-        // Algorithm session;
+        Model model(device);
+        Algorithm session;
 
-        // Algorithm::Types::MatrixNode root;
-        // std::vector<Algorithm::Types::MatrixNode*> matrix_nodes{&root};
+        Algorithm::Types::MatrixNode root;
+        std::vector<Algorithm::Types::MatrixNode*> matrix_nodes{&root};
 
-        // session.run(playouts, 4, device, states, model, matrix_nodes);
+        session.run(playouts, 4, device, states, model, matrix_nodes);
     }
 
     return 0;
